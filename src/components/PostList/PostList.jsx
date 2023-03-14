d47cb2291ef0860e4466211f2e1dc18da3b345f8
marsailifox <milesjf91@gmail.com>
2023-03-14 14:05:07 -0700

    updated routes, changed how posts are handled

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,15 +1,27 @@
-import PostForm from "../PostForm/PostForm";
+import axios from 'axios';
+import PostForm from '../PostForm/PostForm';
 
 function PostList({ posts, addPost, deletePost }) {
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await axios.delete();
+      deletePost(id);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      <PostForm addPost={addPost}/>
-      {posts.map(post => (
+      <PostForm addPost={addPost} />
+      {posts.map((post) => (
         <div key={post.id}>
           <h3>{post.title}</h3>
           <p>{post.body}</p>
           <p>By {post.author}</p>
-          <button onClick={() => deletePost(post.id)}>Delete</button>
+          <button onClick={() => handleDelete(post.id)}>Delete</button>
+          <button>Edit</button>
         </div>
       ))}
     </div>
@@ -17,3 +29,4 @@ function PostList({ posts, addPost, deletePost }) {
 }
 
 export default PostList;
+
